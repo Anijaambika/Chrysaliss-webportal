bfdcbebb6dbdc1707cdc1f024cbcad37acf2f4ed
Anijaambika <37890442+Anijaambika@users.noreply.github.com>
2018-04-11 07:55:58 +1000

    routing and navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HrComponent } from './hr/hr.component';
+import { FinanceComponent } from './finance/finance.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    HrComponent,
+    FinanceComponent
   ],
   imports: [
     BrowserModule,
